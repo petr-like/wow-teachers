8ac4be98cd8e1ecc74cb2d4ddac13dd7388f71c8
Petr F <petr.fedchenko@gmail.com>
2018-10-17 20:29:11 +0300

    fasdf

--- js/news.js
+++ js/news.js
@@ -1,15 +1,18 @@
 $(function(){
-    let news = $.getJSON( "../json/news.json", function() {
+    let news = $.getJSON( "../json/", function() {
         console.log( "success" );
+        console.log(1, news);
     });
     news = news.responseJSON;
 
     let content = '';
-    console.log(news);
+    
 
     // for(let i = 0; i < news.length; i++) {
     //     content += `<div class="title">${news[i].title}</div> <div class="title">${news[i].text}</div>`;
     // }
 
-    $(".content-news").text(content);
+    $(".content-news").click(function() {
+        console.log(2, news);
+    });
 });
