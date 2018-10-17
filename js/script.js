ca1b1fc15d5b20877ebdef6d54dc4598c0eb8c89
Petr F <petr.fedchenko@gmail.com>
2018-10-17 19:48:35 +0300

    New

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $(function(){
-    $(".menu").load("../menu.html"); 
+    $(".content").load("../menu.html");
+
+    $('.changePage').click(function() {
+        $(".content").load("../menu2.html");
+    });
 });
