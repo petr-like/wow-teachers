57ec7ccead517436c68d63cd6b4db5eaeaca22f8
Petr F <petr.fedchenko@gmail.com>
2018-10-17 20:31:10 +0300

    q

--- js/news.js
+++ js/news.js
@@ -1,16 +1,15 @@
 $(function(){
-    let news = $.getJSON( "../json/", function() {
+    let content = '';
+
+    let news = $.getJSON( "../json/news.json", function() {
         console.log( "success" );
+        // for(let i = 0; i < news.length; i++) {
+        //     content += `<div class="title">${news[i].title}</div> <div class="title">${news[i].text}</div>`;
+        // }
         console.log(1, news);
     });
-    news = news.responseJSON;
-
-    let content = '';
     
-
-    // for(let i = 0; i < news.length; i++) {
-    //     content += `<div class="title">${news[i].title}</div> <div class="title">${news[i].text}</div>`;
-    // }
+    news = news.responseJSON;
 
     $(".content-news").click(function() {
         console.log(2, news);
