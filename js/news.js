600423a73c8164a3b3d1c003dd806a8cac7448a3
Petr F <petr.fedchenko@gmail.com>
2018-10-17 20:40:28 +0300

    asd

--- js/news.js
+++ js/news.js
@@ -1,15 +1,13 @@
 $(function(){
     let content = '';
 
-    let news = $.getJSON( "../json/news.json", function() {
+    let news = $.getJSON( "/json/news.json", function() {
         console.log( "success" );
         // for(let i = 0; i < news.length; i++) {
         //     content += `<div class="title">${news[i].title}</div> <div class="title">${news[i].text}</div>`;
         // }
         console.log(1, news);
     });
-    
-    news = news.responseJSON;
 
     $(".content-news").click(function() {
         console.log(2, news);
